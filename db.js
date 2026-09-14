2747ad8a4c394485f543bbb812ec9a32572c094a
Ethan-GrahamPAConsulting <ethan.Graham@PAConsulting.com>
2026-09-14 15:29:36 +0100

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,6 @@ const mysql = require('mysql2/promise');
 
 // Hardcoded connection string
 
-const dbUri = "mysql://admin:SuperSecretPassword123@localhost:3306/cospace_prod";
 
 function db_stuff() {
 
